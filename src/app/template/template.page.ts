c482af7dde366a7261d8bfa2c0ca56c257f2d7c2
Sandro Scalco <sandro.scalco@liitu.ch>
2022-02-19 19:05:26 +0100

    feat cleanup

--- src/app/template/template.page.ts
+++ src/app/template/template.page.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AngularFirestore } from '@angular/fire/compat/firestore'
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  ActivatedRoute
+} from '@angular/router';
+import {
+  AngularFirestore
+} from '@angular/fire/compat/firestore'
 import firebase from 'firebase/compat/app';
-import '@papyrs/stylo';
 
 @Component({
   selector: 'app-template',
@@ -16,30 +22,28 @@ export class TemplatePage implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private afStore: AngularFirestore,
-    ) { 
+  ) {
+    // Your editable element
+    const div = document.querySelector('div[contenteditable="true"]');
 
+    // Stylo
+    const stylo: any = document.querySelector('stylo-editor');
 
-// Your editable element
-const div = document.querySelector('div[contenteditable="true"]');
+    // Set the `containerRef` property
+    stylo.containerRef = div;
 
-// Stylo
-const stylo: any = document.querySelector('stylo-editor');
-
-// Set the `containerRef` property
-stylo.containerRef = div;
-
-    }
+  }
 
   ngOnInit() {
     this.templateName = this.activatedRoute.snapshot.paramMap.get('id');
-    this.getTemplate(this.templateName).then(snapshot=>{
+    this.getTemplate(this.templateName).then(snapshot => {
       console.log(snapshot.data());
       this.template = snapshot.data();
       document.getElementById('htmlpreview').innerHTML = this.template.html;
     });
   }
 
-  getTemplate(id: string){
+  getTemplate(id: string) {
     return this.afStore.collection('template').doc(id).get().toPromise();
   }
 }
